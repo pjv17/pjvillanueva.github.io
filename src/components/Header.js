aefb532cd86f51d67bdedb3264cd8e8127bda99b
pvillanuevatti <peter.villanueva@t>
2023-02-21 09:09:13 +0800

    console error cleanup

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,18 +5,20 @@ import pjlogo from './assets/image/PJ Logo.png'
 
 const Header = () => {
 
-var nav = document.getElementById('site-menu');
-var header = document.getElementById('top');
-
-window.addEventListener('scroll', function() {
-  if (window.scrollY >=400) { // adjust this value based on site structure and header image height
-    nav.classList.add('nav-sticky');
-    header.classList.add('pt-scroll');
-  } else {
-    nav.classList.remove('nav-sticky');
-    header.classList.remove('pt-scroll');
-  }
-});
+let timeout = 1500;
+setTimeout(() => {
+    let navigation = document.querySelector('#site-menu');
+    let topHeader = document.querySelector('#top-header');
+    window.addEventListener('scroll', function() {
+        if (window.scrollY >=400) {     
+            topHeader.classList.add('pt-scroll');
+            navigation.classList.add('nav-sticky');
+        } else {    
+            topHeader.classList.remove('pt-scroll');
+            navigation.classList.remove('nav-sticky');
+        }
+    });
+}, timeout);
 
 /*
 in progress
@@ -34,18 +36,18 @@ function navToggle() {
             <div className="container">
                 <nav id="site-menu" className="flex flex-col sm:flex-row w-full sm:justify-between justify-center items-center px-4 sm:px-6 py-1 bg-white shadow sm:shadow-none border-t-4 border-red-900">
                     <div className="w-full sm:w-auto sm:self-start self-center sm:self-center flex flex-row sm:flex-none flex-no-wrap sm:justify-between justify-center items-center">
-                    <Link className="no-underline py-1" to='/'><h1 className="font-bold text-lg tracking-widest"><img className="pj-logo" src={pjlogo} alt="Peter John Villanueva Logo" /></h1></Link>
+                        <Link className="no-underline py-1" to='/'><h1 className="font-bold text-lg tracking-widest"><img className="pj-logo" src={pjlogo} alt="Peter John Villanueva Logo" /></h1></Link>
                     </div>
                      {/*
+                     In Progress
                      <button id="menuBtn" className="hamburger block sm:hidden focus:outline-none" type="button" onClick={navToggle}>
                         <span className="hamburger__top-bun text-white"></span><span className="hamburger__bottom-bun"></span>
                     </button>
                     </div>
-                   
-                    In Progress
+
                     <div id="menu" className="w-full sm:w-auto self-end sm:self-center sm:flex flex-col sm:flex-row items-center h-full py-1 pb-4 sm:py-0 sm:pb-0 hidden">
-                    <Link className="no-underline py-1 mr-2" to='/'>About</Link>
-                    <Link className="no-underline py-1 mr-2" to='/'>Portfolio</Link>                
+                        <Link className="no-underline py-1 mr-2" to='/'>About</Link>
+                        <Link className="no-underline py-1 mr-2" to='/'>Portfolio</Link>                
                     </div> */}
                 </nav>
             </div>
--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import {BrowserRouter as  Router, Routes, Route} from "react-router-dom";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(
   <Router basename={process.env.PUBLIC_URL}>
     <Routes>
       <Route path ='/' element={<App></App>}>
-
       </Route>
     </Routes>
   </Router>,
-  document.querySelector("#root")
 );
